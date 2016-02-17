b00426915cb5b7265e5db2f50250ec9f38ec32a3
Alex <sleepagain123@163.com>
2016-02-17 17:52:56 +0800

    Signed-off-by: Alex <sleepagain123@163.com>

--- src/main/java/com/liuxue/cms/modules/controller/testController.java
+++ src/main/java/com/liuxue/cms/modules/controller/testController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
@@ -19,7 +20,7 @@ import java.util.Map;
 /**
  * Created by Sandwich on 2016/2/15.
  */
-@Controller
+@RestController
 @RequestMapping("/cms")
 public class testController extends BaseController {
 
